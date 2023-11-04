7e7118845e6ba8db2846a9a04a04d8a12bc51f3b
Tobias Leinß <tobias.leinss@mway.io>
2023-11-04 17:34:56 +0000

    Deploy and verify contract

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomicfoundation/hardhat-verify');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: '0.8.20',
+
+  networks: {
+    polygonMumbai: {
+      chainId: 80001,
+      url: 'https://rpc-mumbai.maticvigil.com',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    url: 'https://mumbai.polygonscan.com',
+    apiKey: process.env.POLYGONMUMBAI_API_KEY,
+  },
 };
